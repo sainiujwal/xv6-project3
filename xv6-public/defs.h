a28babd79a0368c546d55fb60dd89aadf131ee1b
ujwal <ujwalsaini21@gmail.com>
2023-03-24 22:38:58 -0400

    vmprint sys call added

--- xv6-public/defs.h
+++ xv6-public/defs.h
@@ -120,7 +120,6 @@ void            userinit(void);
 int             wait(void);
 void            wakeup(void*);
 void            yield(void);
-int             vmprint(int);
 
 // swtch.S
 void            swtch(struct context**, struct context*);
@@ -186,7 +185,6 @@ void            switchuvm(struct proc*);
 void            switchkvm(void);
 int             copyout(pde_t*, uint, void*, uint);
 void            clearpteu(pde_t *pgdir, char *uva);
-void            print_pagetable(pde_t* pagetable, int level);
 
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
--- xv6-public/user.h
+++ xv6-public/user.h
@@ -23,7 +23,6 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
-int vmprint(int);
 
 // ulib.c
 int stat(const char*, struct stat*);
